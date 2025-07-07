0d740f76625967262a1d2ff1ddfe2582f040547a
RafaelBm5 <rafael.borbamiranda5@gmail.com>
2025-07-07 20:31:38 -0300

    Delete script.js directory

--- script.js/script.js
+++
@@ -1,9 +0,0 @@
-
-document.addEventListener("DOMContentLoaded", () => {
-    const botao = document.getElementById("mensagemBtn");
-    const mensagem = document.getElementById("mensagem");
-
-    botao.addEventListener("click", () => {
-        mensagem.textContent = "Bem-vindo ao meu site! Continue estudando e se dedicando. 💪";
-    });
-});
