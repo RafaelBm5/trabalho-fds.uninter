4c5fb2184de97b74dad185a4c967b28cfc0dfffc
RafaelBm5 <rafael.borbamiranda5@gmail.com>
2025-07-07 20:32:25 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const botao = document.getElementById("mensagemBtn");
+    const mensagem = document.getElementById("mensagem");
+
+    botao.addEventListener("click", () => {
+        mensagem.textContent = "Bem-vindo ao meu site! Continue estudando e se dedicando. 💪";
+    });
+});
